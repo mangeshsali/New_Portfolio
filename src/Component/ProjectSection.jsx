a80d946cff48af40ec9769ded7bae8dafbb8c4c0
Mangesh <mpsali16@gmail.com>
2024-11-09 17:20:04 +0530

    fix:image upload bug

--- src/Component/ProjectSection.jsx
+++ src/Component/ProjectSection.jsx
@@ -1,56 +1,58 @@
 // Projects.jsx
 import React from "react";
-import ProjectCard from "./ProjectCard"; // Adjust the import path as necessary
+import ProjectCard from "./ProjectCard";
 
 const ProjectSection = () => {
   const projects = [
     {
-      image: "/src/assets/sorting.png",
+      image: "https://mangeshsali.netlify.app/src/assets/sorting.png",
       altText: "Sorting Visualizer",
       liveLink: "https://sorting-visualiz.netlify.app/",
       githubLink: "https://github.com/mangeshsali/Sorting_visualizer",
     },
     {
-      image: "/src/assets/Screenshot_20240202_122353.png",
+      image:
+        "https://mangeshsali.netlify.app/src/assets/Screenshot_20240202_122353.png",
       altText: "Python Documentation",
       liveLink: "https://pythondoc.netlify.app/",
       githubLink: "https://github.com/mangeshsali/python-doc",
     },
 
     {
-      image: "/src/assets/news.jpg",
+      image: "https://mangeshsali.netlify.app/src/assets/news.jpg",
       altText: "News App",
       liveLink: "https://newswavee.netlify.app/",
       githubLink: "https://github.com/mangeshsali/News_App",
     },
 
     {
-      image: "/src/assets/changecolor.png",
+      image: "https://mangeshsali.netlify.app/src/assets/changecolor.png",
       altText: "Color Flipper",
       liveLink: "https://flipercolor.netlify.app/",
       githubLink:
         "https://github.com/mangeshsali/OIBSIP/tree/main/Color%20Fliper",
     },
     {
-      image: "/src/assets/bubblegame.jpg",
+      image: "https://mangeshsali.netlify.app/src/assets/bubblegame.jpg",
       altText: "Bubble Game",
       liveLink: "https://thebubblegame.netlify.app/",
       githubLink: "https://github.com/mangeshsali/Bubbble_Game",
     },
     {
-      image: "/src/assets/Screenshot_20240202_123122.png",
+      image:
+        "https://mangeshsali.netlify.app/src/assets/Screenshot_20240202_123122.png",
       altText: "Foodies App",
       liveLink: "https://foodiess.vercel.app/",
       githubLink: "https://github.com/mangeshsali/foodies",
     },
     {
-      image: "src/assets/cinema.png",
+      image: "https://mangeshsali.netlify.app/src/assets/cinema.png",
       altText: "Cinema GPT",
       liveLink: "https://cinemaa-gpt.vercel.app/",
       githubLink: "https://github.com/mangeshsali/CinemaGPT",
     },
     {
-      image: "/src/assets/streamer.png",
+      image: "https://mangeshsali.netlify.app/src/assets/streamer.png",
       altText: "Streamer App",
       liveLink: "https://streamer-eosin.vercel.app/",
       githubLink: "https://github.com/mangeshsali/Strammer",
